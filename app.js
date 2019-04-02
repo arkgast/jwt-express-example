4e78db08f1110b92fbcc639e3c46822a2aa8f29c
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-04-02 11:30:08 -0500

    feat: add verifyToken middleware
    Add a function verifyToken to add the bearer token to the request and check if the bearer header
    exists.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const jwt = require('jsonwebtoken')
-
 const app = express()
 
 app.get('/api', (req, res) => {
@@ -8,6 +6,33 @@ app.get('/api', (req, res) => {
     message: 'Welcome to the API'
   })
 })
+
+app.get('/api/secret', verifyToken, (req, res) => {
+  res.json({
+    message: 'secret information'
+  })
+})
+
+function verifyToken (req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization']
+
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ')
+
+    // Get token from array
+    const bearerToken = bearer[1]
+
+    // Set the token
+    req.token = bearerToken
+
+    next()
+  } else {
+    // Forbindden
+    res.sendStatus(403)
+  }
+}
 
 app.listen(5000, () => {
   console.log('Server started on port 5000')
